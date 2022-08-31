1d47de6182e5c3f9379722789b984070398fff8d
hakareem <harithakareem72@gmail.com>
2022-08-31 15:41:50 +0100

    user repo code

--- lib/user_repository.rb
+++ lib/user_repository.rb
@@ -1,28 +1,82 @@
-require './user.rb'
+require_relative './user'
 
 class UserRepository
-  # Selecting all records
   def all
-    # SELECT id, name, username, email FROM users;
+    sql = 'SELECT id, name, username, email, password FROM users ORDER by id;'
+    result = DatabaseConnection.exec_params(sql, [])
+
+    return get_users(result)
   end
 
-  # Gets a single record by its ID
   def find(id)
-    # SELECT id, name, username, email FROM users WHERE id = $1;
+    sql = 'SELECT * FROM users WHERE id = $1;'
+    result = DatabaseConnection.exec_params(sql, [id])
+
+    return get_users(result)[0]
+  end
+  
+  def find_username(username)
+    sql = 'SELECT * FROM users WHERE username = $1;'
+    result = DatabaseConnection.exec_params(sql, [username])
+
+    return get_users(result)[0]
   end
 
-  # Adds new record to the 'users' table
-  def create(new_user)
-    # INSERT INTO users (name, username, email) VALUES ($1, $2, $3);
+  def find_email(email)
+    sql = 'SELECT * FROM users WHERE email = $1;'
+    result = DatabaseConnection.exec_params(sql, [email])
+
+    return get_users(result)[0]
   end
 
-  # Updates the 'users' table
-  def update(id, col, val)
-    # UPDATE users SET username = $3 WHERE id = $1;
+  def create(user)
+    sql = 'INSERT INTO users (name, username, email, password) VALUES ($1, $2, $3, $4);'
+    params = [user.name, user.username, user.email,user.password]
+    DatabaseConnection.exec_params(sql, params)
   end
 
-  # Removes a record from the 'users' table
-  def delete(username)
-    # DELETE FROM users WHERE username = $1;
+  def update_name(id, name)  
+    sql = 'UPDATE users SET name = $2 WHERE id = $1;' 
+    params = [id, name]
+    DatabaseConnection.exec_params(sql, params)
+  end
+
+  def update_username(id,username)
+    sql = 'UPDATE users SET username = $2 WHERE id = $1;'
+    params = [id, username]
+    DatabaseConnection.exec_params(sql, params)
+  end
+
+  def update_email(id,email)
+    sql = 'UPDATE users SET email = $2 WHERE id = $1;'
+    params = [id, email]
+    DatabaseConnection.exec_params(sql, params)
+  end
+
+  def update_password(id, password)
+    sql = 'UPDATE users SET password = $2 WHERE id = $1;'
+    params = [id, password]
+    DatabaseConnection.exec_params(sql, params)
+  end
+
+  def delete(id)
+    sql = 'DELETE FROM users WHERE id = $1;'
+    DatabaseConnection.exec_params(sql, [id])
+  end
+
+  private
+
+  def get_users(result)
+    users = []
+    result.each do |record|
+      user = User.new
+      user.id = record['id'].to_i
+      user.name = record['name']
+      user.username = record['username']
+      user.email = record['email']
+      user.password = record['password']
+      users << user
+    end
+    return users
   end
 end
