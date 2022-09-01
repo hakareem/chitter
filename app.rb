de51d8bfa699697aee77a58c5da953520b956d55
hakareem <harithakareem72@gmail.com>
2022-09-01 11:15:11 +0100

    added signup route (working)

--- app.rb
+++ app.rb
@@ -26,6 +26,39 @@ class Application < Sinatra::Base
     return erb :signup
   end
 
+  post '/signup' do
+    @users_repo = UserRepository.new
+    email = params[:email]
+    username = params[:username]
+
+    # checking if username OR email exists in db 
+    if @users_repo.find_username(username) == nil && @users_repo.find_email(email) == nil 
+      @user = User.new
+      @user.email = params[:email]
+      @user.username = params[:username]
+      @user.name = params[:name]
+      @user.password = params[:password]
+
+      # only create a user when signup conditions are met
+      if signup_valid?
+        @users_repo.create(@user)
+        # binding.irb
+        return erb :signup_successful
+      end
+
+    # checking if usernames exists in the database
+    elsif @users_repo.find_username(username) != nil 
+      erb :username_exists
+    else 
+      erb :email_exists
+    end
+  end
+
+  # conditions for signup form
+  def signup_valid?
+  return (params[:name].length > 3 && params[:password].length > 6 && params[:username].length > 3 && params[:email].length > 6 && params[:email].include?("@"))
+  end
+
   get '/login' do
     return erb :login
   end
