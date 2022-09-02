9673348abe9449014a80a65feb7d20cb50ff3366
hakareem <harithakareem72@gmail.com>
2022-09-02 11:36:19 +0100

    seeds

--- spec/seeds.sql
+++ spec/seeds.sql
@@ -6,7 +6,7 @@ INSERT INTO users (name, username, email, password) VALUES ('Foe', 'foeez', 'foe
 
 
 INSERT INTO posts (message, timestamp, user_id) VALUES ('Any man who must say, "I am the king," is no true king.', '2022-08-01 12:00:00', 1);
-INSERT INTO posts (message, timestamp, user_id) VALUES ('f you think this has a happy ending, you havent been paying attention.', '2022-07-04 12:00:00', 2);
+INSERT INTO posts (message, timestamp, user_id) VALUES ('If you think this has a happy ending, you havent been paying attention.', '2022-07-04 12:00:00', 2);
 INSERT INTO posts (message, timestamp, user_id) VALUES ('The man who passes the sentence should swing the sword.','2022-06-04 12:00:00', 3);
 
 
